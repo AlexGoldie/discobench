266eb802ea5b6e289a0286fafbb89418f45cdc22
Shashank Chirra <lina4911@engs-32745.local>
2026-02-19 09:54:09 +0000

    minor change to edit/targets

--- discobench/tasks/OnPolicyRL/templates/default/edit/targets.py
+++ discobench/tasks/OnPolicyRL/templates/default/edit/targets.py
@@ -3,4 +3,6 @@ import jax.numpy as jnp
 
 def get_targets(traj_batch, last_val, config):
     """Fill in your logic here, return advantages (for policy learning) and targets (for value learning)"""
+    advantages = ...
+    targets = ...
     return advantages, targets
