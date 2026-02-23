a624a9c192c26e86977906fe698bc55f00505d6d
AlexGoldie <agoldie101@gmail.com>
2026-02-23 11:29:07 +0000

    fixes

--- tests/discobench/utils/test_get_domains.py
+++ tests/discobench/utils/test_get_domains.py
@@ -14,6 +14,7 @@ def test_all_domains_valid_config() -> None:
         "LanguageModelling",
         "ModelUnlearning",
         "OffPolicyRL",
+        "OnPolicyMARL",
         "OnPolicyRL",
         "UnsupervisedEnvironmentDesign",
     ]
